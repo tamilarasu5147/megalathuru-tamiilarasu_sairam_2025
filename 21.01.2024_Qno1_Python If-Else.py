66882f39782196df14c20f0b743c98015a348278
tamilarasu5147 <157339349+tamilarasu5147@users.noreply.github.com>
2024-01-22 10:51:22 +0530

    day1//22-01-2024

---
+++ 21.01.2024_Qno1_Python If-Else.py
@@ -0,0 +1,67 @@
+'''
+Task
+Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird
+Input Format
+
+A single line containing a positive integer, .
+
+Constraints
+
+Output Format
+
+Print Weird if the number is weird. Otherwise, print Not Weird.
+
+Sample Input 0
+
+3
+Sample Output 0
+
+Weird
+'''
+#logic 1
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def main():
+    if n % 2 != 0:
+        print("Weird")
+    elif 2 <= n <= 5:
+        print("Not Weird")
+    elif 6 <= n <= 20:
+        print("Weird")
+    elif n > 20:
+        print("Not Weird")
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    main()
+
+#logic 2
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    if n % 2 == 1 or (n % 2 == 0 and 6 <= n <= 20):
+        print("Weird")
+    elif n % 2 == 0 and (2 <= n <= 5 or n > 20):
+        print("Not Weird")
+
+ 
